0198bab5f1dbeaecc179149fbbda25b62338534e
Marko Ristin <marko.ristin@gmail.com>
2020-07-03 00:39:50 +0200

    Add validation to workflow
    The schemas has been manually tested so far. This change includes
    the scripts to install the programs for schema validation and
    validates the example data in continouous integration.

---
+++ schemas/Validate.ps1
@@ -0,0 +1,51 @@
+<#
+This script validates all the examples against the schemas.
+#>
+
+function Main 
+{
+    Push-Location
+
+    $schemasDir = $PSScriptRoot
+
+    $schemaValidationDir = Join-Path $schemasDir "schema-validation"
+    if(!(Test-Path $schemaValidationDir -PathType Container))
+    {
+        throw "The schema-validation could not be found: $schemaValidationDir; have you installed it?"
+    }
+
+    Set-Location $schemasDir
+
+    
+    Write-Host "Validating JSON ..."
+    & $(Join-Path $schemaValidationDir "ValidateJson") --inputs 'json\examples\*.json' --schema 'json\aas.json'
+    if($LASTEXITCODE -ne 0) 
+    {
+        throw "JSON validation failed."
+    }
+
+
+    Write-Host "Validating RDF ..."
+    & $(Join-Path $schemaValidationDir "ValidateRdf") --model 'rdf\rdf-ontology.ttl'
+    if($LASTEXITCODE -ne 0) 
+    {
+        throw "RDF validation failed."
+    }
+
+    & $(Join-Path $schemaValidationDir "ValidateRdf") --model 'rdf\shacl-schema.ttl'
+    if($LASTEXITCODE -ne 0) 
+    {
+        throw "RDF validation failed."
+    }
+
+
+    Write-Host "Validating XML ..."
+    & $(Join-Path $schemaValidationDir "ValidateXml") --inputs 'xml\examples\*.xml' --schema 'xml\AAS.xsd'
+    if($LASTEXITCODE -ne 0) 
+    {
+        throw "XML validation failed."
+    }
+
+    Pop-Location
+}
+Main
